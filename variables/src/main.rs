828ff5cbc5717de6950881377d6ec711d3f79ac4
KekmaTime <136650032+KekmaTime@users.noreply.github.com>
2023-06-27 22:07:31 +0530

    variables

---
+++ variables/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let u32 = 0;
+    let u64 = 0;
+    let _sucesss = some_function();
+    const MY_CONSTANT:i32 = 99;
+
+
+    //casting
+    let int8:u8 = 255;
+    println!("{}", int8);
+
+    let int16:u16 = int8 as u16;
+    println!("{}", int16);
+}
